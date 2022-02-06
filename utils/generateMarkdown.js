94a5180a237742f283414fddc76b0a0e999dc596
besharpe5 <besharpe15@gmail.com>
2022-02-06 11:51:27 -0500

    created a function that returns a license badge based on which license is passed in - generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// creates a function that returns a license badge based on which license is passed in
+// if there is no license, it returns an empty string
+const renderLicenseBadge = licenseBadge => {
+    if (!licenseBadge) {
+        return '';
+    }
+
+    return `
+        ![License](https://img.shields.io/badge/License-${licenseBadge.split(' ').join(' ')}-brightgreen.svg)
+    `;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
