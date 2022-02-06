2e926fdddb3d0b649e74aeea6d1909986995647d
besharpe5 <besharpe15@gmail.com>
2022-02-06 10:38:02 -0500

    added validations for user input

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
+// creates an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
     {
         type: 'input',
         name: 'username',
-        message: 'What is your GitHub username?'
+        message: 'What is your GitHub username?',
+        validate: (githubInput) => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please type in your GitHub username.');
+            }
+        },
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email address?'
+        message: 'What is your email address?',
+        validate: (emailInput) => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please type in your email address.');
+            }
+        },
     },
     {
         type: 'input',
         name: 'title',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: (titleInput) => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please type in the name of your project.');
+            }
+        },
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Briefly describe your project.'
+        message: 'Briefly describe your project.',
+        validate: (descriptionInput) => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please briefly describe your project.');
+            }
+        },
     },
     {
         type: 'list',
@@ -35,7 +63,14 @@ const questions = () => {
     {
         type: 'input',
         name: 'installation',
-        message: 'What command is needed to install dependencies?'
+        message: 'What command is needed to install dependencies?',
+        validate: (installationInput) => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please include the necesarry commands to install dependencies.');
+            }
+        },
     },
     {
         type: 'input',
@@ -50,16 +85,23 @@ const questions = () => {
     {
         type: 'input',
         name: 'usage',
-        message: 'Please include any additional information required to use this repository.'
+        message: 'Please include any additional information required to use this repository.',
+        validate: (usageInput) => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please include any additional information required to use this repository.');
+            }
+        },
     }
     ])
 };
 
-// TODO: Create a function to write README file
+// creates a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+// creates a function to initialize app
 function init() {}
 
-// Function call to initialize app
+// function call to initialize app
 init();
